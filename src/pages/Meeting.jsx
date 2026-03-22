6e4f4c18df09f36043d55950e718fd048e4a4b31
Rohit Pandit <rpandit0907@gmail.com>
2026-03-22 16:58:55 +0530

    Deployed

--- src/pages/Meeting.jsx
+++ src/pages/Meeting.jsx
@@ -387,7 +387,17 @@ useEffect(() => {
   const createPeer = (socketId, socketInstance) => {
     const peer = new RTCPeerConnection({
       iceServers: [
-        { urls: "stun:stun.l.google.com:19302" },
+        { urls: "stun:stun.relay.metered.ca:80" },
+        {
+          urls: "turn:global.relay.metered.ca:80",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        },
+        {
+          urls: "turn:global.relay.metered.ca:443",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        }
       ],
     });
 
